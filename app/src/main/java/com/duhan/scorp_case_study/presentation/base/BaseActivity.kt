d5a468ef598485293548d13912e778e25d389209
Duhan <dev.duhanbulut@gmail.com>
2022-09-02 23:14:43 +0300

    feat: add required dependencies and base ui classes

---
+++ app/src/main/java/com/duhan/scorp_case_study/presentation/base/BaseActivity.kt
@@ -0,0 +1,47 @@
+package com.duhan.scorp_case_study.presentation.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+
+abstract class BaseActivity<DB : ViewDataBinding, VM : BaseViewModel> : AppCompatActivity() {
+    private lateinit var viewModel: VM
+    private lateinit var binding: DB
+
+    @get:LayoutRes
+    abstract val layoutId: Int
+
+    abstract fun getVM(): VM
+
+    abstract fun bindVM(binding: DB, vm: VM)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        viewModel = getVM()
+        bindVM(binding, viewModel)
+        with(viewModel) {
+            isInProgress.observe(this as LifecycleOwner) {
+                if (it) showProgress()
+                else hideProgress()
+            }
+
+            errorMessage.observe((this as LifecycleOwner)) {
+                Toast.makeText(this@BaseActivity, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun showProgress() = BaseProgress().show(supportFragmentManager, PROGRESS)
+
+    fun hideProgress() =
+        supportFragmentManager.fragments.filterIsInstance<BaseProgress>().forEach { it.dismiss() }
+
+    companion object {
+        private const val PROGRESS = "InProgress"
+    }
+}
---
+++ app/src/main/java/com/duhan/scorp_case_study/presentation/base/BaseViewModel.kt
@@ -0,0 +1,26 @@
+package com.duhan.scorp_case_study.presentation.base
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+    var isInProgress = MutableLiveData<Boolean>()
+    var errorMessage = MutableLiveData<String>()
+
+    inline fun <T> launchPagingAsync(
+        crossinline execute: suspend () -> Flow<T>,
+        crossinline onSuccess: (Flow<T>) -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                val result = execute()
+                onSuccess(result)
+            } catch (ex: Exception) {
+                errorMessage.value = ex.message
+            }
+        }
+    }
+}
